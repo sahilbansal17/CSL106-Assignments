e1e5b2a8ad4278032fc0efda404b01e5e9bf7031
Sahil Bansal <shlbnsl843@gmail.com>
2017-09-16 19:10:50 +0530

    used quick sort to sort by freq in ascending order and made some bug fixes

--- Assignment 2/wordFreqCounter.cpp
+++ Assignment 2/wordFreqCounter.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-#define size 1000 //size of the hashTable
+#define size 10000 //size of the hashTable
 
 //implementation in which we also store the word in the hashtable
 struct hT{
@@ -19,7 +19,7 @@ int hashF(char*s)
 	int index=0;
 	for(int i=0;s[i]!='\0';i++)
 	{
-		index += s[i]-97+1;
+		index += abs(s[i]-97+1); //since it returned -ve index in case of apostrophe
 	}
 	return (index)%size;
 }
@@ -38,7 +38,7 @@ void toLower(char*s)
 void copy(int hashValue,char* modifiedS)
 {
 	int k = 0;
-	hashTable[hashValue].word = new char[101];
+	hashTable[hashValue].word = new char[256];
     while(modifiedS[k]!='\0')
     {
        	hashTable[hashValue].word[k]=modifiedS[k];
@@ -61,6 +61,43 @@ void linearProbe(int probe, char* data)
         }
     }
 }
+
+void swap(hT x[],int a,int b) //to swap two hT structure nodes 
+{
+	int temp = x[a].count;
+	x[a].count = x[b].count;
+	x[b].count = temp;
+
+	char *s = x[a].word;
+	x[a].word = x[b].word;
+	x[b].word = s;
+}
+
+int partArr(hT A[],int s, int e)
+{
+	int i = s+1;
+	int piv = A[s].count;  //choose 1st element's count as pivot
+	for(int j=s+1;j<=e;j++)
+	{
+		if(A[j].count<piv) //as soon as you encounter a smaller no than piv, swap it with the 1st element from beg which is larger than the piv
+		{
+			swap(A,j,i);
+			i++;
+		}
+	}
+	swap(A,s,i-1); //to make sure that pivot element is put in its proper place
+	return i-1;
+}
+
+void quick_sort(hT A[],int beg,int end)
+{
+	if(beg<end)
+	{
+		int pivot_index = partArr(A, beg, end); //partition the array and return the index of element, elements to whose left are smaller and right side are larger than it 
+		quick_sort(A,beg,pivot_index-1); //sort the left partition
+		quick_sort(A,pivot_index+1,end); //sort the right partition
+	}
+}
 int main()
 {
 
@@ -71,12 +108,12 @@ int main()
 	ASCII(') = 39*/ 
 	hashTable[size].count=0; //intialize the count 
 
-	int hashValue, totalWords = 0 ;
+	int hashValue, totalWords = 0 ; //totalWords now refers to the distinct words in the file
 
 	ifstream fin("input.txt"); //to read from a file input.txt
 
 	char* s; //take input word by word from file
-	s = new char[101]; //assuming max size 100 of each word
+	s = new char[256]; //assuming max size 255 of each word
 
 	char delim[] = "! ,.\"#$%&()*+-/:;<>=?@[]\\^_{}|~";
 
@@ -92,6 +129,7 @@ int main()
         	{
         		copy(hashValue,modifiedS);
         		hashTable[hashValue].count++; //increment the count of that word to 1
+        		totalWords ++ ;
         	}
         	else if(strcmp(hashTable[hashValue].word,modifiedS)==0)
 			{
@@ -101,8 +139,10 @@ int main()
         	{
         		//this refers to the case of a collision 
         		linearProbe(hashValue,modifiedS);//resolving using linear probing
-        	} 
-        	totalWords ++ ;
+        		totalWords ++ ;
+        	}
+
+        	
         	modifiedS = strtok(NULL,delim); 
     	}	
 	}
@@ -132,7 +172,7 @@ int main()
 
 	int th; //threshold value
 	cout<<"Please enter the threshold value: ";
-	cin>>th;
+	th = 0;
 
 	cout<<"The total words in the file are: "<<totalWords<<endl;
 
@@ -146,7 +186,7 @@ int main()
 		{
 			reqW ++; //increment count of those words which are greater than threshold
 			int k = 0;
-			thresholdHT[id].word = new char[101];
+			thresholdHT[id].word = new char[256];
     		//copying the word at ith index in hashTable to word at id index in thresholdHT
     		while(hashTable[i].word[k]!='\0')
     		{
@@ -159,10 +199,12 @@ int main()
 		}
 	}
 
+	//sorting the elements in this hashtable from index 0 to reqW-1 in ascending order 
+	quick_sort(thresholdHT,0,reqW-1);
 	if(reqW>0)
 	{
 		cout<<"The words having frequency more than threshold ("<<reqW<<") are:"<<endl;
-		for(int i=0;i<reqW;i++)
+		for(int i=reqW-1;i>=0;i--) //displaying them from the element in last index (largest value of count)
 		{
 			cout<<thresholdHT[i].word<<":"<<thresholdHT[i].count<<endl;
 		}

5d31446ee9200cd8baa67e7341cd0c97834c7d81
Sahil Bansal <shlbns843@gmail.com>
2017-10-18 21:26:29 +0530

    initial commit.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Assignment 4 - 2016CSJ0008
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float R, mu, sigma; //R : arrival rate, mu: mean, sigma: variance of service time
+    int k, N; //k: no of billing counters , N : no of customers
+
+    cin >> R >> k >> mu >> sigma >> N;
+
+    return 0;
+}

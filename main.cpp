03868ffa1c7d7f4dfa7381f7e5b6186f39dc9b54
Rishabh Kumar <rishabh0098@gmail.com>
2017-10-19 03:01:08 +0000

    - Added functions for arrival time generation and service time generation - Changed the header file - Changed some data types

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-//Assignment 4 - 2016CSJ0008
-#include <iostream>
-
+//Assignment 4 - 2016CSJ0008,2016CSJ0013
+#include <bits/stdc++.h>
 using namespace std;
+
+double arrtimegen(double R) {
+	double m = 1/R;
+	std :: default_random_engine generator;
+	std :: uniform_real_distribution<double> distribution(0.0, m);
+	double number = distribution(generator);
+	return number;
+}
+
+double servtimegen(double m, double v) {
+	std :: default_random_engine generator;
+	std :: uniform_real_distribution<double> distribution(m-v, m+v);
+	double number = distribution(generator);
+	return number;
+}
 
 int main() {
 
-    float R, mu, sigma; //R : arrival rate, mu: mean, sigma: variance of service time
+    double R, mu, sigma; //R : arrival rate, mu: mean, sigma: variance of service time
     int k, N; //k: no of billing counters , N : no of customers
 
     cin >> R >> k >> mu >> sigma >> N;

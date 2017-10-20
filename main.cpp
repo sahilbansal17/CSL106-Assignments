d2d52aa3220bdb7f4d624a1119fe613e48052344
Sahil Bansal <shlbns843@gmail.com>
2017-10-20 16:57:26 +0530

    started with H1 and changes made in main function.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 //Assignment 4 - 2016CSJ0008,2016CSJ0013
 #include <iostream>
+#include <vector>
 #include <random>
 
 using namespace std;
@@ -17,12 +18,16 @@ vector<double> arrivalTimeGen(double R, int N) {
     return temp;
 }
 
-double serviceTimeGen(double m, double v) {
+vector<double> serviceTimeGen(double m, double v, int N) {
+    vector<double> temp(N);
     random_device rd;
     mt19937 gen(rd());
     uniform_real_distribution<double> distribution(m - v, m + v);
-    double number = distribution(gen);
-    return number;
+    for (int i = 0; i < N; i++) {
+        double number = distribution(gen);
+        temp[i] = number;
+    }
+    return temp;
 }
 
 class Customer {
@@ -38,14 +43,29 @@ public:
     double ccDepTime; //departure time of its current customer
 };
 
-//heap 2 - customers  
+//heap 2 - customers   
 class H2 {
 
 };
 
 //heap 1 - counters
 class H1 {
+private:
+    Counter *counters;
+public:
+    H1(int k) {
+        counters = new Counter[k];
+        for (int i = 0; i < k; i++) {
+            counters[i].noOfCustomers = 0;
+            counters[i].ccDepTime = 0;
+        }
+    }
 
+    void display(int k) {
+        for (int i = 0; i < k; i++) {
+            cout << counters[i].noOfCustomers << " " << counters[i].ccDepTime << "\n";
+        }
+    }
 };
 
 int main() {
@@ -60,19 +80,23 @@ int main() {
 
     cin >> R >> mu >> sigma >> k >> N;
 
-    vector<double> arrivalTimeVec(N);
-    arrivalTimeVec = arrivalTimeGen(R, N);
+    vector<double> arrivalTimeVec(N), serviceTimeVec(N);
+    //arrivalTimeVec = arrivalTimeGen(R, N);
+    //assuming that the arrival rate is fixed - for time being, can be changed later 
+    double arrTime = 0;
+    for (int i = 0; i < N; i++) {
+        arrivalTimeVec[i] = arrTime;
+        arrTime += R;
+    }
+
     for (int i = 0; i < N; i++) {
         cout << arrivalTimeVec[i] << " ";
     }
-    // TESTING THE SERVICE TIME GENERATION FUNCTION
-    /*
     cout << "\n";
-    for(int i = 0 ; i < N; i++){
-    	cout << serviceTimeGen(mu,sigma) << " ";
-    }
-    */
+    serviceTimeVec = serviceTimeGen(mu, sigma, N); //service time vector
 
+    H1 heap1(k);
+    heap1.display(k);
 
     return 0;
 }

b6688248c1c05bbe2702fb82f2666c71820d3c47
Sahil Bansal <shlbns843@gmail.com>
2017-10-21 16:34:04 +0530

    debugging to be done

--- main.cpp
+++ main.cpp
@@ -35,18 +35,6 @@ public:
     int queueNo;
     double arrivalTime;
     double departureTime;
-
-    Customer(Customer &c1) {
-        queueNo = c1.queueNo;
-        arrivalTime = c1.arrivalTime;
-        departureTime = c1.departureTime;
-    }
-
-    Customer() {
-        queueNo = 0;
-        arrivalTime = 0;
-        departureTime = 0;
-    }
 };
 
 class Counter {
@@ -54,17 +42,6 @@ public:
     int id; 
     int noOfCustomers;
     double ccDepTime; //departure time of its current customer
-    Counter(Counter &c1) {
-        id = c1.id;
-        noOfCustomers = c1.noOfCustomers;
-        ccDepTime = c1.ccDepTime;
-    }
-
-    Counter() {
-        id = 0;
-        noOfCustomers = 0;
-        ccDepTime = 0;
-    }
 };
 
 //heap 2 - customers   
@@ -72,6 +49,45 @@ class H2 {
 private:
     int size; // the current no of customers yet to be served
     Customer *customers;
+
+    int leftChild(int i) {
+        int left = 2 * i + 1;
+        if (left >= size) {
+            return -1;
+        }
+        return left;
+    }
+
+    int rightChild(int i) {
+        int right = 2 * i + 2;
+        if (right >= size) {
+            return -1;
+        }
+        return right;
+    }
+
+    void heapify(int loc) {
+        Customer temp;
+        int l, r, min;
+        l = leftChild(loc);
+        r = rightChild(loc);
+        if (l != -1 && customers[l].departureTime < customers[loc].departureTime) {
+            min = l;
+        } else {
+            min = loc;
+        }
+        if (r != -1 && customers[r].departureTime < customers[min].departureTime) {
+            min = r;
+        }
+        if (min != loc) {
+            //swap the customers[min] with customers[loc]
+            temp = customers[min];
+            customers[min] = customers[loc];
+            customers[loc] = temp;
+            heapify(min);
+        }
+        return;
+    }
 public:
     H2(int N) {
         customers = new Customer[N]; //at most there can be N customers in the shop
@@ -89,6 +105,34 @@ public:
                  << "\n";
         }
     }
+
+    void insert(Customer c) {
+        size++;
+        int index = size - 1;
+        //parent is at (index-1)/2 since starting from 0th index
+        while (index > 0 && c.departureTime < customers[(index - 1) / 2].departureTime) {
+            customers[index] = customers[(index - 1) / 2];
+            index = (index - 1) / 2;
+        }
+        customers[index] = c; //right position for this customer 
+    }
+
+    double topCustomerDepTime() {
+        return customers[0].departureTime;
+    }
+
+    double topCustomerQueueNo() {
+        return customers[0].queueNo;
+    }
+
+    void deleteMin() {
+        customers[0] = customers[size - 1];
+        size--;
+        //call heapify 
+        heapify(0);
+        return;
+    }
+
 };
 
 //heap 1 - counters
@@ -114,24 +158,24 @@ private:
 
     void heapify(int loc) {
         Counter temp;
-        int l, r, max;
+        int l, r, min;
         l = leftChild(loc);
         r = rightChild(loc);
-        if (l != -1 && counters[l].noOfCustomers > counters[loc].noOfCustomers) {
-            max = l;
+        if (l != -1 && counters[l].noOfCustomers < counters[loc].noOfCustomers) {
+            min = l;
         } else {
-            max = loc;
+            min = loc;
         }
-        if (r != -1 && counters[r].noOfCustomers > counters[loc].noOfCustomers) {
-            max = r;
+        if (r != -1 && counters[r].noOfCustomers < counters[min].noOfCustomers) {
+            min = r;
         }
-        if (max != loc) {
-            //swap counters[loc] with counters[max]
+        if (min != loc) {
+            //swap counters[loc] with counters[min]
             temp = counters[loc];
-            counters[loc] = counters[max];
-            counters[max] = temp;
+            counters[loc] = counters[min];
+            counters[min] = temp;
+            heapify(min);
         }
-        heapify(max);
         return;
     }
 public:
@@ -147,28 +191,70 @@ public:
 
     void display(int k) {
         for (int i = 0; i < k; i++) {
-            cout << counters[i].noOfCustomers << " " << counters[i].ccDepTime << "\n";
+            cout << counters[i].id << " " << counters[i].noOfCustomers << " " << counters[i].ccDepTime << "\n";
         }
     }
 
-    void insert(double arrival, double service) {
+    Customer insert(double arrival, double service) {
+        int Q_NO = counters[0].id; //counter no
+        double depT;
         //if there is no customer in the queue
         if (counters[0].noOfCustomers == 0) {
             counters[0].noOfCustomers++;
             counters[0].ccDepTime = arrival + service;
+            depT = counters[0].ccDepTime;
             //heapify the counters heap based on the no of customers
             heapify(0);
         }
             //else if there are customers in the queue, then the 0th location will contain the counter which has the least no of customers
         else {
+
+            //case 1 : the arrival time is more than the time when service of the current customer will be finished
+            /*if(arrival > counters[0].ccDepTime){
+                noOfCustomers -- ; 
+                //delete the previous customer 
+                --> 
+                counters[0].ccDepTime = arrival + service;
+            }*/
+            //above case will already be handled using the delete min operation in the heap 2
+
+            //case 2 : the arrival time is less than the time when service of the current customer will be finished
             counters[0].noOfCustomers++;
             counters[0].ccDepTime = counters[0].ccDepTime +
                                     service; //might need to store the departure time of all the customers in the queue
+            depT = counters[0].ccDepTime;
             //heapify the counters heap
             heapify(0);
         }
+        //need to add this element into the heap 2, so need to return its arrival,departure time and the queue no
+        Customer c;
+        c.queueNo = Q_NO;
+        c.arrivalTime = arrival;
+        c.departureTime = depT;
+        return c;
+
     }
 
+    void updateHeap(int qN) {
+        //find the appropriate location out of the k counters
+        int id;
+        for (int i = 0; i < size; i++) {
+            if (counters[i].id == qN) {
+                id = i;
+                break;
+            }
+        }
+        //id contains the index at which we need to update the noOfCustomers
+        counters[id].noOfCustomers--;
+        int noc = counters[id].noOfCustomers;
+        Counter c = counters[id];
+        //parent is at (id-1)/2 since starting from 0th index
+        while (id > 0 && noc < counters[(id - 1) / 2].noOfCustomers) {
+            counters[id] = counters[(id - 1) / 2];
+            id = (id - 1) / 2;
+        }
+        counters[id] = c; //right position for this customer 
+    }
 };
 
 int main() {
@@ -198,6 +284,11 @@ int main() {
     cout << "\n";
     serviceTimeVec = serviceTimeGen(mu, sigma, N); //service time vector
 
+    serviceTimeVec[0] = 5.8;
+    serviceTimeVec[1] = 6.3;
+    serviceTimeVec[2] = 6.9;
+    serviceTimeVec[3] = 3.5;
+    serviceTimeVec[4] = 6.5;
     for (int i = 0; i < N; i++) {
         cout << serviceTimeVec[i] << " ";
     }
@@ -210,9 +301,26 @@ int main() {
 
     double clock = 0;
     int currentLoc = 0;
-    /*while(clock != -1){
+    while (clock != -1 && currentLoc < N) {
         clock+= arrivalTimeVec[currentLoc];
-        heap1.insert(clock, serviceTimeVec[currentLoc]);
-    }*/
+        //case 1: when the clock is larger than the dep time of customer on top of H1
+        //keep deleting customers till the dep time of top customer is smaller than clock
+        if (clock > heap2.topCustomerDepTime()) {
+            int qn; //to find the correct counter in H1
+            //-->call deleteMin till the topCustomerDepTime is smaller than clock
+            while (heap2.topCustomerDepTime() < clock) {
+                qn = heap2.topCustomerQueueNo();
+                heap2.deleteMin();
+                //--> we also need to update the queue no in H1 by reducing its no of customers
+                heap1.updateHeap(qn);
+            }
+        }
+            //otherwise simply generate a node from H1 and insert in H2
+        else {
+            heap2.insert(heap1.insert(clock, serviceTimeVec[currentLoc]));
+        }
+        currentLoc++;
+    }
+    heap1.display(k);
     return 0;
 }
